5181f23df3a82187872440a4e26b8bf6f8d0fd97
gllmsb <142408342+gllmsb@users.noreply.github.com>
2024-09-09 09:55:56 +0200

    mobile query

--- lexi/src/layouts/MainLayout.jsx
+++ lexi/src/layouts/MainLayout.jsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../components/Navbar/Navbar";
-import styles from './MainLayout.module.scss';
 import { Header } from "../components/Header/Header";
 import { Footer } from "../components/Footer/Footer";
+import styles from './MainLayout.module.scss';
+
 
 
 export const MainLayout = () => {
 
     return(
-        <>
+        <>  
         <Navbar/>
         <Header/>
         <main className={styles.mainStyle}>
